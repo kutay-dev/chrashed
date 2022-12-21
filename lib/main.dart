a923c72ddf7e6b2c2f92185db52f7c382ca612f2
evolade <kutaycereci89@gmail.com>
2022-12-21 23:28:19 +0300

    add pull to refresh to leaderboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:math';
 import 'dart:ui';
 import 'package:firebase_core/firebase_core.dart';
@@ -318,45 +319,53 @@ class _MainState extends State<Main> {
                         if (snapshot.connectionState == ConnectionState.done) {
                           final List<DocumentSnapshot> documents =
                               snapshot.data!.docs;
-                          return ListView.builder(
-                            itemCount: documents.length,
-                            itemBuilder: (context, i) {
-                              return Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(70, 7, 70, 7),
-                                child: ListTile(
-                                  visualDensity:
-                                      const VisualDensity(vertical: -3),
-                                  shape: RoundedRectangleBorder(
-                                    side: const BorderSide(width: 2),
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
-                                  dense: true,
-                                  tileColor: (box.read("userId") ==
-                                          documents[i]["id"])
-                                      ? Colors.white10
-                                      : const Color.fromARGB(255, 12, 12, 12),
-                                  leading: Text(
-                                    (i + 1).toString(),
-                                    style:
-                                        const TextStyle(color: Colors.white38),
-                                  ),
-                                  title: Text(documents[i]["name"].toString()),
-                                  subtitle: Text('${documents[i]["alc"]} ml'),
-                                  trailing: (box.read("userId") ==
-                                          documents[i]["id"])
-                                      ? IconButton(
-                                          onPressed: () =>
-                                              confirmDelete(documents[i]["id"]),
-                                          icon: const Icon(
-                                            Icons.delete,
-                                            color: Colors.white24,
-                                          ),
-                                        )
-                                      : null,
-                                ),
-                              );
+                          return RefreshIndicator(
+                            color: Colors.black,
+                            onRefresh: () {
+                              setState(() {});
+                              return Future.delayed(const Duration(seconds: 0));
                             },
+                            child: ListView.builder(
+                              itemCount: documents.length,
+                              itemBuilder: (context, i) {
+                                return Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(70, 7, 70, 7),
+                                  child: ListTile(
+                                    visualDensity:
+                                        const VisualDensity(vertical: -3),
+                                    shape: RoundedRectangleBorder(
+                                      side: const BorderSide(width: 2),
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                    dense: true,
+                                    tileColor: (box.read("userId") ==
+                                            documents[i]["id"])
+                                        ? Colors.white10
+                                        : const Color.fromARGB(255, 12, 12, 12),
+                                    leading: Text(
+                                      (i + 1).toString(),
+                                      style: const TextStyle(
+                                          color: Colors.white38),
+                                    ),
+                                    title:
+                                        Text(documents[i]["name"].toString()),
+                                    subtitle: Text('${documents[i]["alc"]} ml'),
+                                    trailing: (box.read("userId") ==
+                                            documents[i]["id"])
+                                        ? IconButton(
+                                            onPressed: () => confirmDelete(
+                                                documents[i]["id"]),
+                                            icon: const Icon(
+                                              Icons.delete,
+                                              color: Colors.white24,
+                                            ),
+                                          )
+                                        : null,
+                                  ),
+                                );
+                              },
+                            ),
                           );
                         }
 
